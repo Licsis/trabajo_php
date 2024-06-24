010bde2d598a534a88e45137f5fad7687f596a23
Juan <juan.marquez@educacionadventista.org.ar>
2024-06-24 14:28:46 -0300

    nuevo trabajo hecho

---
+++ trabajos_varios/TP_ejercicios/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <fieldset>
+            <legend>Tus Datos</legend>
+            <input type="text" name="nom_usuario" id="">
+            <input type="number" name="salario" id="">            
+            <input type="number" name="anio" id="">
+            <input type="submit" value="Mensaje">
+
+            
+        </fieldset>
+
+    </form>
+</body>
+</html>
