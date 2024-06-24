b2880a17421a705e9d4da1791f5f37cb48ca8a13
Juan <juan.marquez@educacionadventista.org.ar>
2024-06-24 17:14:00 -0300

    Se agrega nuevo trabajo

--- trabajos_varios/TP_ejercicios/index.php
+++ trabajos_varios/TP_ejercicios/index.php
@@ -3,20 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style2.css">
     <title>Ejercicios</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <fieldset>
-            <legend>Tus Datos</legend>
-            <input type="text" name="nom_usuario" id="">
-            <input type="number" name="salario" id="">            
-            <input type="number" name="anio" id="">
-            <input type="submit" value="Mensaje">
-
-            
-        </fieldset>
-
-    </form>
+    <div class="container">
+        <section>
+            <form action="index.php" method="post">
+                <fieldset>
+                    <legend>Tus Datos</legend>
+                    <input type="text" name="nom_usuario">
+                    <input type="number" name="salario">            
+                    <input type="number" name="anio">
+                    <input type="submit" value="Mensaje">                    
+                </fieldset>
+            </form>
+        </section>
+        <section>
+            <h1>Se visualizarán los datos</h1>
+        </section>
+    </div>
+    <script src="../js/script.js"></script>
 </body>
 </html>
--- trabajos_varios/tabla_multiplicar/index.php
+++ trabajos_varios/tabla_multiplicar/index.php
@@ -8,30 +8,30 @@
     <title>Tabla de Multiplicar</title>
 </head>
 <body>
-<div class="container">
-    <section>  
-        <br>
-        <form action="index.php" method="post">
-            <input type="number" name="num1" placeholder="Digitá el número para obtener su tabla de multimplicar">
-            <input type="submit" value="Obtener Tabla">
-        </form>
-    </section>
-    <section class="formato_texto fecha md_style">
-        <?php
-        //El sistema requiere ingresar la edad del usuario, verificar si es mayor de edad y mostrar por pantalla la siguente leyenda: es mayor de edad, de lo contrario es menor de edad, teniendo en cuenta que la mayoría de edad es cuando tiene 18 o más años.
+    <div class="container">
+        <section>  
+            <br>
+            <form action="index.php" method="post">
+                <input type="number" name="num1" placeholder="Digitá el número para obtener su tabla de multimplicar">
+                <input type="submit" value="Obtener Tabla">
+            </form>
+        </section>
+        <section class="formato_texto fecha md_style">
+            <?php
+            //El sistema requiere ingresar la edad del usuario, verificar si es mayor de edad y mostrar por pantalla la siguente leyenda: es mayor de edad, de lo contrario es menor de edad, teniendo en cuenta que la mayoría de edad es cuando tiene 18 o más años.
 
-        //En base al ejercicio anterior, incorporar lo siguiente, si es mayor de edad y tiene la estatura menor o igual a 170 centímetros, mostrar por pantalla el siguiente mensaje "Puede ingresar a la montaña rusa", de lo contrario "No puede ingresar". 
+            //En base al ejercicio anterior, incorporar lo siguiente, si es mayor de edad y tiene la estatura menor o igual a 170 centímetros, mostrar por pantalla el siguiente mensaje "Puede ingresar a la montaña rusa", de lo contrario "No puede ingresar". 
 
-        //Se necesita que el usuario ingrese su salario, nombre y años trabajando en una empresa, se desea mostrar sus datos por pantalla con el siguiente formato ->  "El Señor NOMBRE, Cuenta con un salarido de: SALARIO; y lleva trabajando para nosotros AÑOS_TRABAJADOS Años".
-        if (!empty($_POST)) {
-            $valor = $_POST['num1'];
-            for ($i=1; $i <= 10; $i++) { 
-                echo''. $valor .'*'. $i ." = ".$i*$valor. "<br><br>";
+            //Se necesita que el usuario ingrese su salario, nombre y años trabajando en una empresa, se desea mostrar sus datos por pantalla con el siguiente formato ->  "El Señor NOMBRE, Cuenta con un salarido de: SALARIO; y lleva trabajando para nosotros AÑOS_TRABAJADOS Años".
+            if (!empty($_POST)) {
+                $valor = $_POST['num1'];
+                for ($i=1; $i <= 10; $i++) { 
+                    echo''. $valor .'*'. $i ." = ".$i*$valor. "<br><br>";
+                }
             }
-        }
-        ?>
-    </section>
-    <script src="js/script.js"></script>
+            ?>
+        </section>
+        <script src="js/script.js"></script>
     </div>
 </body>
 </html>
